2436c9a77806cf60c867734b0f2e0d66ba1b9cc8
Ayush <itsayushdwivedi@gmail.com>
2017-08-12 07:06:19 +0530

    Almost done

--- explainxkcdbot.py
+++ explainxkcdbot.py
@@ -8,6 +8,9 @@ from bs4 import BeautifulSoup
 from urllib.parse import urlparse
 
 
+para_list = []
+
+
 def authenticate():
     print("Authenticating...")
     reddit = praw.Reddit('explainbot', user_agent = "xkcd explain bot")
@@ -25,17 +28,31 @@ def run_explainbot(reddit):
     print("Getting 250 comments...")
     
     for comment in reddit.subreddit('test').comments(limit=250):
-        matches = re.findall("^https://www.xkcd.com/[0-9]+", comment.body)
-        if matches:
+        match = re.findall("^https://www.xkcd.com/[0-9]+", comment.body)
+        if match:
             print("String found in comment " + comment.id)
-            url = matches[0]
-            urlobj = urlparse(url)
-            xkcd_id = int((urlobj.path.strip("/")))
-            print(xkcd_id)
-            #comment.reply("Found...")
+            xkcd_url = match[0]
+            url_obj = urlparse(xkcd_url)
+            xkcd_id = int((url_obj.path.strip("/")))
+            url = 'http://www.explainxkcd.com/wiki/index.php/' + str(xkcd_id)
 
-    print("Sleeping for 10 seconds...")
-    time.sleep(5)
+            r = requests.get(url)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            
+            start = soup.find('table')
+
+            for i in start.find_next_siblings('p'):
+                para = i.get_text()
+                para_list.append(para)
+                if(i.next_sibling.name != 'p'):
+                    break
+
+            text = "\n".join(para_list)
+            #print(text)
+            comment.reply(text)
+
+    print("Sleeping for 60 seconds...")
+    time.sleep(60)
 
 
 if __name__ == '__main__':
--- extractdata.py
+++ extractdata.py
@@ -3,22 +3,25 @@ from bs4 import BeautifulSoup
 
 
 xkcd_id = 1024
-url_string = 'http://www.explainxkcd.com/wiki/index.php/' + str(xkcd_id)
+url = 'http://www.explainxkcd.com/wiki/index.php/' + str(xkcd_id)
+para_list = []
 
 
-r = requests.get(url_string)
+r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
 
 
 def main():
     start = soup.find('table')
     
-    for a in start.find_next_siblings('p'):
-        s = a.get_text()
-        print(s)
-        if(a.next_sibling.name != 'p'):
+    for i in start.find_next_siblings('p'):
+        para = i.get_text()
+        para_list.append(para)
+        if(i.next_sibling.name != 'p'):
             break 
 
+    text = "\n".join(para_list)
+    print(text)
 
 if __name__ == '__main__':
     main()
