ec73f53950c511fe9c4eb8083a60d9467bdb48fe
Ayush <itsayushdwivedi@gmail.com>
2017-08-13 03:54:59 +0530

    Final changes

--- extractdata.py
+++ extractdata.py
@@ -1,11 +1,17 @@
+# An independent program to simply fetch and display explanation of an xkcd comic strip
+# The explanation is extracted from http://explainxkcd.com
+# Created by Ayush Dwivedi (/u/kindw)
+# License: MIT License
+
+
 import requests
 import bs4
 from bs4 import BeautifulSoup
 
-
 xkcd_id = 1024
-url = 'http://www.explainxkcd.com/wiki/index.php/' + str(xkcd_id)
+# Replace xkcd_id with the serial number of xkcd you want the explanation for
 
+url = 'http://www.explainxkcd.com/wiki/index.php/' + str(xkcd_id)
 
 r = requests.get(url)
 soup = BeautifulSoup(r.content, 'html.parser')
@@ -26,6 +32,7 @@ def main():
         else:
             tag = tag.nextSibling
     print (data)
+
 
 if __name__ == '__main__':
     main()
